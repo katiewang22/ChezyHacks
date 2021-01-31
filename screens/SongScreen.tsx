8ae9ae9b1959d44d0c139e7a17d7b4a0a605c8a7
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-01-30 21:11:39 -0500

    Song Screen Update

--- screens/SongScreen.tsx
+++ screens/SongScreen.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function SongScreen() {
-  return (
+import Constant from 'expo-constants'
+import { WebView } from 'react-native-webview';
+
+export default function SongScreen({route}){
+  /*return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
   );
-}
+}*/
 
 const styles = StyleSheet.create({
   container: {
@@ -27,6 +30,41 @@ const styles = StyleSheet.create({
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
-  },
-});
+    width: '80%', 
+  }}
+
+//const, SongScreen = ({route: string})=>{
+    const{videoId,title} = route.params
+   return(
+       <View style={{
+           flex:1,
+        marginTop:Constant.statusBarHeight
+        }}>
+           <View style={{
+               width:"100%",
+               height:200
+           }}>
+              <WebView
+              javaScriptEnabled={true}
+              domStorageEnabled={true}
+               source={{uri:`https://www.spotif.com/embed/${videoId}`}}
+              />
+
+           </View>
+           <Text style={{
+               fontSize:20,
+               width:Dimensions.get("screen").width - 50,
+               margin:9
+           }}
+           numberOfLines={2}
+           ellipsizeMode="tail"
+           >
+             {title}
+           </Text>
+           <View
+             style={{borderBottomWidth:1}}
+           />
+       </View>
+   )
+}
+
